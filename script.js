7c49fdd3875ff1591e20f9fcc1bb21c4e3913376
Ivana Janostikova <ivkagre@gmail.com>
2024-10-07 19:01:38 +0200

    ukol_ockovani

---
+++ script.js
@@ -0,0 +1,3 @@
+const  jmeno = prompt("Ako sa voláte", "meno a priezvisko")
+const vek = prompt("Koľko máte rokov", "18")
+document.body.innerHTML = "<p>Pacientove meno je: " + jmeno + " a má " + vek + " rokov </p>" 
